89aed644acf6c200a10508997b3d4aac0df01ff5
Ben Harman <mrb.harman@gmail.com>
2018-12-06 21:36:57 +0000

    Add service

--- ContentUpdateAI.Web/src/app/app.component.ts
+++ ContentUpdateAI.Web/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../app/services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -16,15 +17,20 @@ export class AppComponent {
     this.luisResult = "Tadaaaa!";
   }
 
-  constructor(private http: HttpClient) {
-  }
-
   public GetLuisResult(luisQuery: any) {
-    this.luisResult = this.http.get("/contentupdate/v1/check-composite", { params: { query: luisQuery.value["luis-query"] } })
-      .subscribe(result => { this.luisResult = JSON.stringify(result) },
-        error => console.log(error));
+    this.apiService.getData('/contentupdate/v1/check-composite', luisQuery).subscribe((data: Array<object>) => {
+      this.luisResult = data;
+      console.log(data);
+    });
+    //this.luisResult = this.http.get("/contentupdate/v1/check-composite", { params: { query: luisQuery.value["luis-query"] } })
+    //  .subscribe(result => { this.luisResult = JSON.stringify(result) },
+    //    error => console.log(error));
 
     console.log("Result: ", this.luisResult);
   }
+
+  constructor(private http: HttpClient, private apiService: ApiService) {
+  }
+
 
 }
---
+++ ContentUpdateAI.Web/src/app/services/api.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Response, RequestOptions } from "@angular/http";
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  extractData(res: Response) {
+    let body = res.json();
+    return body || {};
+  }
+
+  handleError(error: any): any {
+    return Observable.throw(error.statusText);
+  }
+
+  postData(url: string, postObj: any) {
+    return this.httpClient.post(url, postObj, httpOptions)
+      .pipe(catchError(this.handleError(postObj))
+    );
+  }
+
+  getData(url: string, postQuery: string) {
+    return this.httpClient.get(url, { params: { query: postQuery } });
+  }
+
+  constructor(private httpClient: HttpClient) {
+  }
+}
